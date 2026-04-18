a4888da5b5ec58e052be666fb1ab6ce1b5c78ad7
saadelnady <saad.elnady.work@gmail.com>
2026-04-18 21:36:07 +0200

    Fix frontend auth

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -3,7 +3,10 @@ import { API_URL } from "./apiUrl";
 export const register = async (username, password, role) => {
   const res = await fetch(`${API_URL}/register`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "ngrok-skip-browser-warning": "69420", // السطر السحري
+    },
     body: JSON.stringify({ username, password, role }),
   });
   return res.json();
@@ -12,7 +15,10 @@ export const register = async (username, password, role) => {
 export const login = async (username, password) => {
   const res = await fetch(`${API_URL}/login`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "ngrok-skip-browser-warning": "69420", // السطر السحري
+    },
     body: JSON.stringify({ username, password }),
   });
   return res.json();
