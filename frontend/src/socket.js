53aa7a8205142045c118789c5945cad2fbbf17bd
saadelnady <saad.elnady.work@gmail.com>
2026-04-18 21:12:50 +0200

    refactor

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 export const url =
   process.env.NODE_ENV === "production"
-    ? undefined
-    : process.env.REACT_APP_API_URL;
+    ? process.env.REACT_APP_API_URL
+    : "http://localhost:3000";
 
 export const socket = io(url, {
   autoConnect: true,
